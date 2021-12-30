7b7fc4b129a7ae3ecbba9086df41e4c2467b5d55
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-12-30 23:53:05 +0100

    feat: Create an interafce for the block parser to store and retrieve data about blocks

---
+++ src/main/kotlin/com/github/recraftedcivilizations/IParseData.kt
@@ -0,0 +1,51 @@
+package com.github.recraftedcivilizations
+
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Store and get data about blocks that have
+ * to be regenerated
+ */
+interface IParseData {
+
+    /**
+     * Set a block in the data
+     * @param block The block that should respawn after a delay
+     * @param respawnTime The time that is left till the block should respawn
+     */
+    fun storeBlock(block: Block, respawnTime: Int)
+
+    /**
+     * Delete a block from the storage
+     * @param blockLocation The location of the block that should be removed
+     */
+    fun removeBlock(blockLocation: Location)
+
+    /**
+     * Get the type of a stored block
+     * @param blockLocation The location of the block
+     * @return The Material the block should respawn as
+     */
+    fun getBlockType(blockLocation: Location): Material
+
+    /**
+     * Get the respawn time of a block
+     * @param blockLocation The location of the block
+     * @return The respawn time that is left
+     */
+    fun getRespawnTime(blockLocation: Location): Int
+
+    /**
+     * Set the respawn time of a block
+     * @param blockLocation The location of the block
+     * @param respawnTime The new respawn time
+     */
+    fun setRespawnTime(blockLocation: Location, respawnTime: Int)
+
+}
