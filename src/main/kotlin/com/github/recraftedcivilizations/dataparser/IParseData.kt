0de710453002e6ece6850bc53831b73bd5f8b9bb
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-12-31 11:14:19 +0100

    refractor: Move the data parser interace to the dataparser package

---
+++ src/main/kotlin/com/github/recraftedcivilizations/dataparser/IParseData.kt
@@ -0,0 +1,15 @@
+package com.github.recraftedcivilizations.dataparser
+
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Store and get data about blocks that have
+ * to be regenerated
+ */
+interface IParseData : IParseBlocks, IParseTimes
