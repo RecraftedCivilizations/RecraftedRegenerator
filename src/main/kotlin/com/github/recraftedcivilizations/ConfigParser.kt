d823842a090a2e6e7b32e06d9f5cd4108663a15a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-12-31 10:43:37 +0100

    feat: Implement a config parser

---
+++ src/main/kotlin/com/github/recraftedcivilizations/ConfigParser.kt
@@ -0,0 +1,57 @@
+package com.github.recraftedcivilizations
+
+import org.bukkit.Material
+import org.bukkit.configuration.Configuration
+import org.bukkit.configuration.ConfigurationSection
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+fun Configuration.getMaterial(path: String, def: Material): Material{
+    val strMat = this.getString(path) ?: return  def
+    return Material.matchMaterial(strMat) ?: return def
+}
+
+class ConfigParser(private val config: Configuration) {
+    var respawnTimes: MutableMap<Material, Int> = emptyMap<Material, Int>().toMutableMap()
+    var defaultDelay: Int? = null
+    var emptyBlock: Material? = null
+
+    /**
+     * Load data from a configuration
+     */
+    fun load(){
+
+        // Create a new empty mutable map so we don't use the old config values again
+        respawnTimes = emptyMap<Material, Int>().toMutableMap()
+        // Get the default delay for every material if not defined explicitly
+        defaultDelay = config.getInt("defaultDelay", 0)
+        // Get the material the block should show up as when mined
+        emptyBlock = config.getMaterial("empty", Material.BEDROCK)
+
+        // Get the Config section with all materials and delays
+        val delaySection: ConfigurationSection = config.getConfigurationSection("delays")
+            ?: throw NoSuchElementException("Please define a delay section in your config file!!")
+
+        // Loop through all listed materials
+        for (materialKey in delaySection.getKeys(false)){
+
+            // Get the material from the string key
+            val material: Material = Material.matchMaterial(materialKey)
+                ?: throw IllegalArgumentException("The supplied material $materialKey does not exist as material!!")
+
+            // Get the section of the material
+            val materialSection = delaySection.getConfigurationSection(materialKey)!!
+            // Get the delay of the material if none is set put the default delay in
+            val delay = materialSection.getInt("${materialKey}.delay", defaultDelay!!)
+
+            // Add the material to the respawn types
+            respawnTimes[material] = delay
+
+        }
+
+    }
+
+
+}
