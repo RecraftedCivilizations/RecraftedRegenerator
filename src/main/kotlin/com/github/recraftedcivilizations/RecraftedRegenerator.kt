a7d2c870d8331185736ffd610fae649701da3d2c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-12-31 16:35:56 +0100

    feat: Call the regenerator on enable

--- src/main/kotlin/com/github/recraftedcivilizations/RecraftedRegenerator.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/RecraftedRegenerator.kt
@@ -6,6 +6,7 @@ import com.github.recraftedcivilizations.dataparser.blockparser.YAMLBlockParser
 import com.github.recraftedcivilizations.dataparser.timeparser.CachedTimeParser
 import com.github.recraftedcivilizations.listeners.BlockBreakListener
 import com.github.recraftedcivilizations.listeners.BlockPlaceListener
+import com.github.recraftedcivilizations.runnables.Regenerator
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -23,5 +24,10 @@ class RecraftedRegenerator: JavaPlugin() {
 
         Bukkit.getPluginManager().registerEvents(BlockBreakListener(dataParser, configParser), this)
         Bukkit.getPluginManager().registerEvents(BlockPlaceListener(regenCommand, dataParser, configParser), this)
+
+
+        val regenerator = Regenerator(dataParser, configParser)
+        regenerator.runTaskTimer(this, 0L, configParser.interval!!.toLong())
+
     }
 }
