1e89d42dad0714e0f99c314a35b7f787c230fa6f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-12-31 16:33:37 +0100

    feat: Create the runnable that regenerates the ores

---
+++ src/main/kotlin/com/github/recraftedcivilizations/runnables/Regenerator.kt
@@ -0,0 +1,30 @@
+package com.github.recraftedcivilizations.runnables
+
+import com.github.recraftedcivilizations.ConfigParser
+import com.github.recraftedcivilizations.dataparser.DataParser
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.scheduler.BukkitRunnable
+
+class Regenerator(private val dataParser: DataParser, private val configParser: ConfigParser): BukkitRunnable() {
+
+    override fun run() {
+
+        val toRespawn = dataParser.getBlocksToRespawn()
+
+        for((location, time) in toRespawn.entries){
+
+            val shouldRespawn = dataParser.setRespawnTime(location, time - configParser.interval!!)
+
+            if(shouldRespawn){
+                val material = dataParser.getBlockType(location) ?: continue
+                respawn(location, material)
+
+            }
+        }
+    }
+
+    private fun respawn(location: Location, type: Material){
+        location.block.type = type
+    }
+}
