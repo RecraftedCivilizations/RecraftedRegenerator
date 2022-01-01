ff4c2ef81b93c59dd42db6e16dfdfec0f1a6aa85
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2022-01-01 14:13:40 +0100

    feat: Test the BlockPlaceListener

---
+++ src/test/kotlin/com/github/recraftedcivilizations/listeners/BlockPlaceListenerTest.kt
@@ -0,0 +1,55 @@
+package com.github.recraftedcivilizations.listeners
+
+import com.github.recraftedcivilizations.ConfigParser
+import com.github.recraftedcivilizations.commands.PlaceRegenOre
+import com.github.recraftedcivilizations.dataparser.IParseData
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.event.block.BlockPlaceEvent
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BlockPlaceListenerTest {
+    private lateinit var blockPlaceEvent: BlockPlaceEvent
+    private lateinit var placeRegenOre: PlaceRegenOre
+    private lateinit var dataParser: IParseData
+    private lateinit var configParser: ConfigParser
+    private lateinit var block: Block
+
+
+    @BeforeEach
+    fun setUp() {
+        blockPlaceEvent = mockk(null, true)
+        placeRegenOre = mockk(null, true)
+        dataParser = mockk(null, true)
+        configParser = mockk(null, true)
+        block = mockk(null, true)
+    }
+
+    @Test
+    fun onBlockPlaceEvent() {
+
+        every { placeRegenOre.isInRegenMode(any()) } returns true
+        every { configParser.respawnTimes.keys } returns setOf(Material.IRON_ORE).toMutableSet()
+        every { blockPlaceEvent.block } returns block
+        every { block.type } returns Material.IRON_ORE
+
+        val l = BlockPlaceListener(placeRegenOre, dataParser, configParser)
+
+        l.onBlockPlaceEvent(blockPlaceEvent)
+
+
+        verify { placeRegenOre.isInRegenMode(any()) }
+        verify { block.type }
+        verify { configParser.respawnTimes.keys }
+        verify { dataParser.storeBlock(block) }
+        confirmVerified(placeRegenOre, configParser, dataParser)
+
+    }
+}
